bb095dc41ed18484c370ba0fd4d2f7010c22959b
Nolan Reda <nolan.reda@gmail.com>
2022-03-03 07:27:15 +0000

    completed for loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log(' ');
+
+for (let i = 0; i < 20; i++ + i++) {
+  console.log(i);
+}
+
+console.log(' ');
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion:', i);
+}
